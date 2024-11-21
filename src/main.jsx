9787725d4f673421d3a42696efe55ba713300a0d
thalissonsouzas <thalisson.snt@gmail.com>
2024-11-20 22:39:32 -0300

    adicionado extensao vercel speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { GlobalStateProvider } from './context/GlobalStateContext'; // Importe o provider
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStateProvider>
       <App />
+      <SpeedInsights />
     </GlobalStateProvider>
   </React.StrictMode>,
 )
